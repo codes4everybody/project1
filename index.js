bb3b2453083ee68e6a563371cf489fe3abf2bb65
Haziqa Khan <139248947+codes4everybody@users.noreply.github.com>
2025-03-17 02:01:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var randomNumber1 = Math.floor(Math.random()*6) +1 ;
+
+var randomImages = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImages);
+
+
+var randomNumber2 = Math.floor(Math.random()*6) +1 ;
+
+var randomImages2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImages2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚀Player 1 Wins!" 
+    ;}
+    else if (randomNumber2 > randomNumber1){
+        document.querySelector("h1").innerHTML = "🚀Player 2 Wins!"
+    }
+    else{
+        document.querySelector("h1").innerHTML = "🚀Draw!"
+    }
+
+
+
+
+
+
+
